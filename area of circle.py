4a821522e8d2da5db3db89c36862885ce4426038
Hemanth1611 <108987709+Hemanth1611@users.noreply.github.com>
2022-07-14 18:05:40 +0530

    Update and rename circle.py to area of circle.py

--- area of circle.py
+++ area of circle.py
@@ -5,3 +5,9 @@ radius=float(input("Enter the radius of the cirle :"))
 area=math.pi*radius*radius
 
 print("Area of the circle is :{0}".format(area))
+
+
+
+filename=input("Input the Filename:")
+
+print("the extention of file is :"+repr(f_exts[-1]))
